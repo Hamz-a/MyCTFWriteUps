ba1aa4ed49b791b339935193495ff607374d681e
Hamz-a <dzcyberdev@gmail.com>
2016-03-04 15:16:57 +0100

    added code 60

---
+++ internetwache_2016/code60_Its Prime Time/code60.php
@@ -0,0 +1,39 @@
+<?php
+
+//$address = '127.0.0.1';
+$address = '192.168.56.101';
+$port = 1337;
+
+
+echo "Creating a socket...\n";
+
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === false) {
+    echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
+} else {
+    echo "OK.\n";
+}
+
+echo "Connecting to $address : $port...\n";
+
+$result = socket_connect($socket, $address, $port);
+if ($result === false) {
+    echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
+} else {
+    echo "OK.\n";
+}
+
+while ($out = socket_read($socket, 2048)) {
+    echo "Read $out \n";
+    if(preg_match('/prime number after (\d+)/i', $out, $m)) {
+        $gmp_next_prime = gmp_nextprime($m[1]);
+        $next_prime = gmp_strval($gmp_next_prime);
+        
+        echo "Writing $next_prime\n";
+        socket_write($socket, $next_prime, strlen($next_prime));
+    }
+    sleep(0.5);
+}
+
+
+socket_close($socket);
---
+++ internetwache_2016/code60_Its Prime Time/readme.md
@@ -0,0 +1,20 @@
+# Code 60 : It's Prime Time!
+
+**Description:**
+
+We all know that prime numbers are quite important in cryptography. Can you help me to find some?
+
+# Writeup
+
+We got an IP address + port. A simple nc got us the following output:
+
+```
+Hi, you know that prime numbers are important, don't you? Help me calculating the next prime!
+Level 1.: Find the next prime number after 5:
+```
+
+If you have completed some [Project Euler](https://projecteuler.net) challenges, this might be actually quite easy. Instead of using some fancy algorithm to find those primes, I used the [gmp library](http://php.net/manual/en/book.gmp.php) which has [`gmp_nextprime()`](http://php.net/manual/en/function.gmp-nextprime.php) to find the next prime.
+
+After executing this [simple script](code60.php):
+
+![1](files/1.png?raw=true)
